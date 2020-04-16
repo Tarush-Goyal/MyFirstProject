6f48995e3e17259db05115f08615c710f19a4f45
Tarush Goyal <tarush@Tarushs-MacBook-Air.local>
2020-04-16 23:28:41 +0530

    menu fixed, working on sidenav/menu content

--- src/app/menu/books-menu/books-menu.component.ts
+++ src/app/menu/books-menu/books-menu.component.ts
@@ -1,29 +1,36 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatMenuTrigger } from '@angular/material/menu';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatMenuTrigger } from "@angular/material/menu";
 
 @Component({
-  selector: 'app-books-menu',
-  templateUrl: './books-menu.component.html',
-  styleUrls: ['./books-menu.component.scss']
+  selector: "app-books-menu",
+  templateUrl: "./books-menu.component.html",
+  styleUrls: ["./books-menu.component.scss"]
 })
 export class BooksMenuComponent implements OnInit {
+  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+  enterBook=false;
+  enterMenu=false;
 
-@ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
-
-enter(){
-  this.trigger.openMenu();
-  console.log('entered');
-}
-
-exit(){
-
-  this.trigger.toggleMenu();
-  console.log('exit');
-}
-
-  constructor() { }
-
-  ngOnInit(): void {
+  Book() {
+    this.trigger.openMenu();
+    this.enterBook=true;
   }
 
+  exitBook() {
+    this.enterBook=false;
+    setTimeout (() => {
+      if(this.enterMenu===false)
+      {this.trigger.closeMenu();}
+    }, 50);
+
+  }
+
+  exitMenu() {
+    this.trigger.closeMenu();
+    this.enterMenu=false;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
